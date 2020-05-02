41b8581c5f57781dab9767da86de450707f6024c
tsbarrett89 <tsbarrett89@gmail.com>
2020-05-02 00:16:04 -0600

    removed typeDefs and brought them in from schema file

--- src/index.js
+++ src/index.js
@@ -1,22 +1,5 @@
 const { GraphQLServer } = require('graphql-yoga')
 
-const typeDefs = `
-    type Query {
-        info: String!
-        feed: [Link!]!
-    }
-
-    type Mutation {
-        post(url: String!, description: String!): Link!
-    }
-
-    type Link {
-        id: ID!
-        description: String!
-        url: String!
-    }
-`
-
 let links = [{
     id: 'link-0',
     url: 'www.howtograpql.com',
@@ -36,7 +19,7 @@ const resolvers = {
 }
 
 const server = new GraphQLServer ({
-    typeDefs,
+    typeDefs: './schema.graphql',
     resolvers,
 })
 

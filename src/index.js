b192da2619e39454f6656a4e866940d587b19eb9
tsbarrett89 <tsbarrett89@gmail.com>
2020-05-03 08:44:11 -0600

    subscription imported

--- src/index.js
+++ src/index.js
@@ -5,12 +5,14 @@ const Query = require('./resolvers/Query')
 const Mutation = require('./resolvers/Mutation')
 const User = require('./resolvers/User')
 const Link = require('./resolvers/Link')
+const Subscription = require('./resolvers/Subscription')
 
 const resolvers = {
     Query,
     Mutation,
     User,
-    Link
+    Link,
+    Subscription
 }
 
 const server = new GraphQLServer ({
